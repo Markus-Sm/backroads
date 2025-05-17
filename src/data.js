2a86254691fea89caf10c177ec46cc2bda74c294
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2025-05-17 21:53:34 +0200

    update

--- src/data.js
+++ src/data.js
@@ -89,7 +89,7 @@ export const tour = [
         text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
         location: "indonesia",
         days: "11 days",
-        price: "from $1405"
+        price: "from $1406"
     },
     {
         id: 3,
