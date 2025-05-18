3fad4f0af4f95aa938e8badf0d1b1b0a96ffa662
Marek Samofał <96687413+Markus-Sm@users.noreply.github.com>
2025-05-18 16:10:02 +0200

    footer component 02

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,25 +14,19 @@ const Footer = () => {
         })}
       </ul>
       <ul className="footer-icons">
-        <li>
-          <a href="https://www.twitter.com" target="_blank" className="footer-icon"
-            ><i className="fab fa-facebook"></i
-          ></a>
-        </li>
-        <li>
-          <a href="https://www.twitter.com" target="_blank" className="footer-icon"
-            ><i className="fab fa-twitter"></i
-          ></a>
-        </li>
-        <li>
-          <a href="https://www.twitter.com" target="_blank" className="footer-icon"
-            ><i className="fab fa-squarespace"></i
-          ></a>
-        </li>
+        {socialLinks.map((socialLink) => {
+          const {id, href, icon } = socialLink;
+          return (
+            <li key={id}>
+            <a className="footer-icon" href={href} target="_blank"
+              ><i className={icon}></i></a>
+          </li>
+          )
+        })}
       </ul>
       <p className="copyright">
         copyright &copy; Backroads travel tours company
-        <span id="date"></span> all rights reserved
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
       </p>
     </footer>
     )
